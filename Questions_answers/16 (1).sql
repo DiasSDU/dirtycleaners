00cd25da3ada06a59aea902be9d0afa63109c5a2
t1ma <58632896+Timowko@users.noreply.github.com>
2021-03-13 23:05:32 +0600

    Rename 16 (1).sql to 16.sql

--- Questions_answers/16 (1).sql
+++
@@ -1,2 +0,0 @@
-/*average number of floors of all apartments*/
-select avg(Total_floors) from House_Price_Astana;
---
+++ Questions_answers/16.sql
@@ -0,0 +1,2 @@
+/*average number of floors of all apartments*/
+select avg(Total_floors) from House_Price_Astana;
