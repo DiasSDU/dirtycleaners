dfe332dbfd714fb28befbd1375d8ceab2ce28710
t1ma <58632896+Timowko@users.noreply.github.com>
2021-03-13 23:06:28 +0600

    Rename 10 (2).sql to 10.sql

--- Questions_answers/10 (2).sql
+++
@@ -1,2 +0,0 @@
-/* What is the price range in a certain area*/
-select price from House_prices_astana where street = 'Мангилик Ел 17';
---
+++ Questions_answers/10.sql
@@ -0,0 +1,2 @@
+/* What is the price range in a certain area*/
+select price from House_prices_astana where street = 'Мангилик Ел 17';
