994edcd0b066bc81dfa0e296e9b61c7d91c381b5
DiasSDU <49234217+DiasSDU@users.noreply.github.com>
2021-03-13 23:00:16 +0600

    Rename 1 (1).sql to 1.sql

--- Questions_answers/1 (1).sql
+++
@@ -1,2 +0,0 @@
-/*Count the average area for the 3-room apartments located in Алматы district and built between 2005 and 2010.*/
-select avg(area) from HOUSE_PRICES_ASTANA where rooms = 3 and district = 'Алматы р-н' and year between 2005 and 2010;
