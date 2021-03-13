1d009425c959e44fd23b5acb03052dba8b1943ff
t1ma <58632896+Timowko@users.noreply.github.com>
2021-03-13 23:19:43 +0600

    Update 11.sql

--- Questions_answers/11.sql
+++ Questions_answers/11.sql
@@ -1,2 +1,2 @@
 /* What is the price range in a certain area*/
-select price from House_prices_astana where street = 'Мангилик Ел 17';
+select min(price),max(price) from House_prices_astana district = 'Есиль р-н';
