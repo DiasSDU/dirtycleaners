3f4f8f69ba21ef4603eb7b03c0bf30bb66339b51
DiasSDU <49234217+DiasSDU@users.noreply.github.com>
2021-03-13 23:01:17 +0600

    Rename 4 (1).sql to 4.sql

--- Questions_answers/4 (1).sql
+++
@@ -1,2 +0,0 @@
-/*Difference of average prices of 4-room and 5-room apartments, with floor less than 10, brick type and built from 2019.*/
-select avg(price) from House_prices_astana where (rooms =5 or rooms =4) and floor<10 and type='кирпичный дом' and year>=2019;
