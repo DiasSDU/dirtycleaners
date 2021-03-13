7f193026bbaabaf3f6803b98b392687ebec8a532
DiasSDU <49234217+DiasSDU@users.noreply.github.com>
2021-03-13 23:01:59 +0600

    Rename 6 (1).sql to 6.sql

--- Questions_answers/6 (1).sql
+++
@@ -1,2 +0,0 @@
-/*Max price of apartments grouping by floor in Esil district with type-monolite, and built before 2010.*/
-select max(price) from House_Price_Astana where district = 'Есиль р-н' and type='монолитный дом' and year<2010 group by floor;
