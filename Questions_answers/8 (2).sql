7292fffcf266b1053c3189b7b160d4569ce35c57
t1ma <58632896+Timowko@users.noreply.github.com>
2021-03-13 23:01:31 +0600

    Add files via upload

--- Questions_answers/8 (2).sql
+++ Questions_answers/8 (2).sql
@@ -1,2 +1,2 @@
-/*How to find an apartment in the right price range*/
-select * from House_prices_astana where price between 3000000 and 6000000;
+/*How to find an apartment according to the required parameters*/
+select * from House_prices_astana where price between 3000000 and 6000000 and city = 'Нур-Султан (Астана)';
