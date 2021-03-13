4a84abee75098c39f2afb9df95751f87e5ad57e8
DiasSDU <49234217+DiasSDU@users.noreply.github.com>
2021-03-13 23:20:26 +0600

    Update 10.sql

--- Questions_answers/10.sql
+++ Questions_answers/10.sql
@@ -1,2 +1,2 @@
-/* Is it possible this year to save m	oney when buying an apartment*/
+/* Is it possible this year to save money when buying an apartment*/
 select price from House_prices_astana where year = 2016 and city = 'Нур-Султан (Астана)';
